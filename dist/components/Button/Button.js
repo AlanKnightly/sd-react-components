79d1c1b98d5ad28cb3a5ba2dda8221b72ed13e77
shenjiangping <shenjiangping@sdbattery.com>
2020-12-14 08:49:22 +0800

    feat: setup sd-components

---
+++ dist/components/Button/Button.js
@@ -0,0 +1,3 @@
+import * as React from "react";
+var Button = function () { return React.createElement("span", { className: "test" }, "test"); };
+export default Button;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+
+const Button = () => <span className="test">test</span>;
+export default Button;
